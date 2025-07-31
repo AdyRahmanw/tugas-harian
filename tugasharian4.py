25c0d8228a58bc73c5bf3f4a1a64c6e23b9350f5
AdyRahmanw <adyrahmanw123@gmail.com>
2025-08-01 01:24:14 +0700

    Add files via upload

--- tugasharian4.py
+++ tugasharian4.py
@@ -1,7 +1,25 @@
-def input_login():
-    Username = input(str("masukan nama =  "))
-    
+DataLogin = {
+    'admin': '1234',
+    'user1': 'abcd',
+    'user2': 'pass123'
+}
 
+def cek_login():
+    percobaan = 0
+    while percobaan < 3:
+        username = input("Masukan Username: ")
+        password = input("Masukan Password: ")
+        percobaan += 1
 
-def cek_login(username, password, data_akun):
-    
+        if username in DataLogin:
+            if password == DataLogin[username]:
+                return "Login berhasil"
+            else:
+                print(f"password salah")
+        else:
+            print("username salah")
+
+    return "Gagal, akun terkunci."
+
+hasil = cek_login()
+print(hasil)
